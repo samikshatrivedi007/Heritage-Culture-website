03dea6f396835db48f82f056bd329b8a85b62ef2
Samiksha <samikshatrivedi007@gmail.com>
2025-05-21 01:58:26 +0530

    credentials updated

--- server/src/libs/mongoose.ts
+++ server/src/libs/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/indian-heritage';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://samikshatrivedi007:Tanu@123@cluster0.mvzm1nn.mongodb.net/';
 
 let isConnected = false;
 
