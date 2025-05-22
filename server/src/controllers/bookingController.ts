559c4423aecebd92c6739d6bf791f24c88f2a713
Samiksha <samikshatrivedi007@gmail.com>
2025-05-22 23:57:28 +0530

    booking code updated

--- server/src/controllers/bookingController.ts
+++ server/src/controllers/bookingController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { Booking } from '../models/booking';
+import ShoppingItem from "../models/shoppingItem";
 
 export const createBooking = async (req: Request, res: Response) => {
     try {
@@ -17,5 +18,47 @@ export const getAllBookings = async (_req: Request, res: Response) => {
         res.status(200).json(bookings);
     } catch (error) {
         res.status(500).json({ error: 'Could not retrieve bookings' });
+    }
+};
+
+export const getBookingById = async (req: Request, res: Response) => {
+    try {
+        const getBooking = await Booking.findById(req.params.id);
+        if (!getBooking){
+            res.status(404).json({ error: 'Booking not found' })
+            return
+        }
+        res.status(200).json(getBooking);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch booking' });
+    }
+};
+
+export const updateBooking = async (req: Request, res: Response) => {
+    try {
+        const updatedBooking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedBooking){
+            res.status(404).json({ error: 'Item not found' })
+            return
+        };
+        {
+            res.status(200).json(updatedBooking);
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to update item' });
+    }
+};
+
+export const deleteBooking = async (req: Request, res: Response) => {
+    try {
+        const deletedBooking = await Booking.findByIdAndDelete(req.params.id);
+        if (!deletedBooking) {
+            res.status(404).json({ error: 'Item not found' })
+            return
+        };
+        res.status(200).json({ message: 'Item deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to delete item' });
     }
 };
--- server/src/routes/bookingRoutes.ts
+++ server/src/routes/bookingRoutes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createBooking, getAllBookings } from '../controllers/bookingController';
+import {
+    createBooking,
+    deleteBooking,
+    getAllBookings,
+    getBookingById,
+    updateBooking
+} from '../controllers/bookingController';
 
 const router = express.Router();
 
 router.post('/', createBooking);
 router.get('/', getAllBookings);
+router.get('/:id', getBookingById);
+router.delete('/:id', deleteBooking);
+router.put('/:id', updateBooking);
 
 export default router;
